2d91ae118d0bbce28a5680f6a30cc0f3ba734a6f
GowrisGit <gowris.dev@gmail.com>
2021-05-10 15:34:37 +1000

    props destructuring

--- src/index.js
+++ src/index.js
@@ -2,24 +2,41 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const title = "How to Compete and Win with Artificial Intelligence";
-const author = "Author: Ash Fontana";
-const img = "https://m.media-amazon.com/images/I/41sGqA-HQiL.jpg";
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/41sGqA-HQiL.jpg",
+  title: "How to Compete and Win with Artificial Intelligence",
+  author: "Author: Ash Fontana",
+};
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/41r7nyZ-SrS.jpg",
+  title:
+    "Trucking Business Startup:Start,Run,Scale a Profitable Trucking Company",
+  author: "Ryan Butler",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt="AI Book" width="200" height="300" />
-      <h3>{title}</h3>
-      <h4>{author.toUpperCase()} </h4>;
+      <h1>{title}</h1>
+      <h5>{author} </h5>
     </article>
   );
 };
